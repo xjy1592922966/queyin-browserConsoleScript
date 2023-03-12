5c1622460d0bf14b26440b1baa9c2c796d76f479
xjy <1592922966@qq.com>
2023-03-12 20:10:49 +0800

    雀鹰项目正式启动， 需要后续补充简单原型图、tapd看板。用于管理需求和进度；

---
+++ src/popup/components/App.vue
@@ -0,0 +1,67 @@
+
+
+<template>
+    <a-layout class="layout">
+        <a-layout-header>
+            <div class="logo">
+                <img src="@/assets/logo/logo256.png" alt="">
+            </div>
+            <a-menu v-model:selectedKeys="selectedKeys" theme="dark" mode="horizontal" :style="{ lineHeight: '64px' }">
+                <a-menu-item key="1">脚本</a-menu-item>
+                <a-menu-item key="2">监控</a-menu-item>
+                <a-menu-item key="3">报表</a-menu-item>
+                <a-menu-item key="3">工具</a-menu-item>
+            </a-menu>
+        </a-layout-header>
+        <a-layout-content style="padding: 0 50px">
+            <a-breadcrumb style="margin: 16px 0">
+                <a-breadcrumb-item>首页</a-breadcrumb-item>
+                <a-breadcrumb-item>脚本</a-breadcrumb-item>
+                <a-breadcrumb-item>chrome插件</a-breadcrumb-item>
+            </a-breadcrumb>
+            <div :style="{ background: '#fff', padding: '24px', minHeight: '380px' }">
+                <a-button type="primary">脚本测试</a-button>
+                <a-button type="primary">脚本测试</a-button>
+                <a-button type="primary">脚本测试</a-button>
+                <a-button type="primary">脚本测试</a-button>
+                <a-button type="primary">脚本测试</a-button>
+                <a-button type="primary">脚本测试</a-button>
+                <a-button type="primary">脚本测试</a-button>
+                <a-button type="primary">脚本测试</a-button>
+                <a-button type="primary">脚本测试</a-button>
+                <a-button type="primary">脚本测试</a-button>
+                <a-button type="primary">脚本测试</a-button>
+                <a-button type="primary">脚本测试</a-button>
+                <a-button type="primary">脚本测试</a-button>
+                <a-button type="primary">脚本测试</a-button>
+            </div>
+        </a-layout-content>
+        <a-layout-footer style="text-align: center">
+            <!-- 雀鹰 ©2023 夏继业 -->
+            希望你永远年轻，永远热泪盈眶
+        </a-layout-footer>
+    </a-layout>
+</template>
+  
+<script>
+export default {
+
+}
+</script>
+  
+<style scoped lang="scss"> 
+
+.logo {
+     height: 64px;
+     width: 34px;
+     float: left;
+     margin: 0px 20px;
+     img {
+         display: inline-block;
+         width: 34px;
+         height: 34px;
+     }
+
+ }
+</style>
+  
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,116 @@
 const { defineConfig } = require('@vue/cli-service')
+const Timestamp = new Date().getTime()
+
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const path = require("path");
+
+// 复制文件到指定目录
+const copyFiles = [
+  {
+    from: path.resolve("src/plugins/manifest.json"),
+    to: `${path.resolve("dist")}/manifest.json`
+  },
+  {
+    from: path.resolve("src/assets"),
+    to: path.resolve("dist/assets")
+  },
+  {
+    from: path.resolve("src/plugins/inject.js"),
+    to: path.resolve("dist/js")
+  },
+  {
+    from: path.resolve("src/plugins/jquery-3.5.1.min.js"),
+    to: path.resolve("dist/js")
+  }
+];
+
+// 复制插件
+const plugins = [
+  new CopyWebpackPlugin({
+    patterns: copyFiles
+  })
+];
+
+// 页面文件
+const pages = {};
+// 配置 popup.html 页面
+const chromeName = ["popup"];
+
+chromeName.forEach(name => {
+  pages[name] = {
+    entry: `src/${name}/main.js`,
+    template: `src/${name}/index.html`,
+    filename: `${name}.html`
+  };
+});
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+
+  chainWebpack(config) {
+    config.optimization.minimize(false);
+  }
+  ,
+  css: {
+    loaderOptions: {
+      // 你的基础样式 因为没有我就注释了
+      // sass: {
+      //     data: `
+      // 		@import "@/assets/style/base.scss";
+      // 	`,
+      // },
+      //这只主题样式，修改此文件后需要重新启动，
+      less: {
+        lessOptions: {
+          modifyVars: {
+            //这是配置css主题色
+            'primary-color': '#007AFF',
+          },
+          javascriptEnabled: true,
+        },
+      },
+    },
+    // 每次打包后生成的css携带时间戳
+    extract: {
+      filename: `css/[name].${Timestamp}.css`,
+      chunkFilename: `css/[name].${Timestamp}.css`,
+    },
+  },
+  //打包后相对目录
+  // publicPath: process.env.NODE_ENV === 'production' ? '././' : './',
+  configureWebpack: {
+    //每次打包后生成的js携带时间戳
+    output: {
+      filename: `[name].${Timestamp}.js`,
+      chunkFilename: `[name].${Timestamp}.js`,
+    },
+  },
+  //修改Webpack配置
+  configureWebpack:{
+
+resolve: {
+  // 配置路径别名
+  alias: {
+    '@': path.resolve(__dirname, './src'),
+  },
+},
+  },
+  transpileDependencies: true,
+  pages,
+  productionSourceMap: false,
+  // 配置 content.js background.js
+  configureWebpack: {
+    entry: {
+      background: "./src/background/main.js"
+    },
+    output: {
+      filename: "js/[name].js"
+    },
+    plugins
+  },
+  // 配置 content.css
+  css: {
+    extract: {
+      filename: "css/[name].css"
+    }
+  }
+});
